7c1f218161ac169956d0d9a3a6c014f013a132bb
M. ZIDAN RURIANO. AG <zidanruriano@gmail.com>
2025-11-23 15:54:50 +0700

    nambah komponen ScrollToTop supaya saat user pindah halaman, halamannya paling atas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Detail from './pages/Detail'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Detail from "./pages/Detail";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
@@ -15,7 +17,7 @@ function App() {
         <Route path="/kos/:slug" element={<Detail />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
