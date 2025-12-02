48c2b1e3102fd725c542d9e7b5747806460b503b
M. ZIDAN RURIANO. AG <zidanruriano@gmail.com>
2025-12-02 19:18:34 +0700

    perbaiki dikit

--- apps/client/vite.config.js
+++ apps/client/vite.config.js
@@ -5,15 +5,13 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: "public",
-  envDir: "../",
   resolve: {
     alias: {
-      "/src": path.resolve(process.cwd(), "src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   build: {
-    outDir: "../dist",
+    outDir: "dist",
     emptyOutDir: true,
   },
 });
